24ab897ed78e4ba817bedf7b23a9ca23fa1d2288
IDEASORIGINALES\miguel.barbosa <miguel.barbosa@ioinvestigacion.com>
2026-03-02 14:59:36 +0100

    Cambios realizados

--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import LoginForm
+from .models import UserRole, Role
 
-# Create your views here.
+
+#Generamos la funcion de login view
+
+def login_view(request):
+    if request.user.is_authenticated: # Si el usuario esta autenticado, lo lleva al dashboard
+        return redirect('dashboard')
+    
+    if request.method == 'POST': # si no esta autenticado, compara el usuario y el password con la bbdd y si es correcto redirecciona a dashboard
+
+        form = LoginForm(request, data-request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = autheticate(usernaem-username, password-password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.error(request, 'Invalid username or password')
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+# Vamos a llamar al logout para ello como requerimiento es que este logado
+
+@login_required
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have successfully logged out.')
+    return redirect('login')
